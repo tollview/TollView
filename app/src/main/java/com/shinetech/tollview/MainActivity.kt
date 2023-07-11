584c64c917288d4b23a5c3884af62dfed496bc41
gdknas <mckevitt.r@gmail.com>
2023-07-11 17:30:15 -0500

    added sign out button

--- app/src/main/java/com/shinetech/tollview/MainActivity.kt
+++ app/src/main/java/com/shinetech/tollview/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.shinetech.tollview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSignOut: Button
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSignOut = findViewById(R.id.btnSignOut)
+
+        btnSignOut.setOnClickListener{
+            btnSignOut.isClickable = false
+            auth.signOut()
+            woof("User, ", "GOODBYE")
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+    }
+    private fun toastln(s: String) {
+        Toast.makeText(
+            applicationContext,
+            s,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+    private fun woof(name: String, s: String) {
+        println("$name: $s")
+        toastln("$name: $s")
     }
 }
