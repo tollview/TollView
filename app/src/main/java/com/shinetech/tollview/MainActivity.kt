43c78f58b3f52688be0140b0aaec33a815789031
gdknas <mckevitt.r@gmail.com>
2023-08-18 00:42:31 -0500

    removed all unneeded debug buttons, functionality, and textViews

--- app/src/main/java/com/shinetech/tollview/MainActivity.kt
+++ app/src/main/java/com/shinetech/tollview/MainActivity.kt
@@ -3,21 +3,10 @@ package com.shinetech.tollview
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.Sampler.Value
 import android.widget.Button
 import androidx.core.app.ActivityCompat
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.shinetech.tollview.models.Gate
-import com.shinetech.tollview.models.Toll
-import com.shinetech.tollview.models.User
 import com.shinetech.tollview.util.Utility
-import java.sql.Timestamp
-import kotlin.random.Random
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
@@ -32,9 +21,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var utility: Utility
 
     private lateinit var btnSignOut: Button
-    private lateinit var btnDebugGetAllGates: Button
-    private lateinit var btnDebugGiveUserToll: Button
-    private lateinit var btnDebugAssignRandomToll: Button
 
     lateinit var tvTollTerminal: TextView
     lateinit var tvTodayTotalCost: TextView
@@ -46,12 +32,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var tvReentryTimeValue: TextView
     lateinit var tvPingSpeedValue: TextView
     lateinit var btnUpdateValues: Button
-
-    private var gatesList: ArrayList<Gate> = ArrayList<Gate>()
-
-    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-    private val gatesReference: DatabaseReference = database.reference.child("gates")
-    private val usersReference: DatabaseReference = database.reference.child("users")
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
@@ -97,9 +77,6 @@ class MainActivity : AppCompatActivity() {
         utility = Utility(applicationContext)
 
         btnSignOut = findViewById(R.id.btnSignOut)
-        btnDebugGetAllGates = findViewById(R.id.btnDebugGetAllGates)
-        btnDebugGiveUserToll = findViewById(R.id.btnDebugGiveDummyTolls)
-        btnDebugAssignRandomToll = findViewById(R.id.btnDebugAssignRandomToll)
 
         tvTodayTotalCost = findViewById(R.id.tvTodayTotalCost)
 
@@ -134,21 +111,6 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }
-
-
-        btnDebugGetAllGates.setOnClickListener{
-            getAllGatesFromDatabase()
-        }
-
-        btnDebugGiveUserToll.setOnClickListener {
-            println("Current User ID: ${auth.currentUser?.uid}")
-
-            addUserToDatabase()
-        }
-
-        btnDebugAssignRandomToll.setOnClickListener{
-            assignRandomToll()
         }
 
         val filter = IntentFilter()
@@ -199,131 +161,6 @@ class MainActivity : AppCompatActivity() {
         })
 
     }
-
-    private fun addTextToTerminal(s: String) {
-        tvTollTerminal.append("\n$s")
-    }
-
-    private fun getAllGatesFromDatabase() {
-        gatesReference.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                for (eachGate in snapshot.children) {
-                    val gate: Gate? = eachGate.getValue(Gate::class.java)
-                    gate?.let {
-                        gatesList.add(gate)
-                    }
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-
-        })
-    }
-
-    private fun addUserToDatabase() {
-
-        val id: String = auth.currentUser?.uid ?: return
-
-        println("Generated ID: $id")
-        println("Auth ID: ${auth.currentUser?.uid}")
-
-        val tolls = ArrayList<Toll>()
-
-        val timestamp = Timestamp(234827042)
-        val toll = Toll("dummyString", timestamp)
-        tolls.add(toll)
-        tolls.add(toll)
-        tolls.add(toll)
-
-        val user = User(tolls)
-
-        println("User: $user")
-
-        usersReference.child(id).setValue(user).addOnCompleteListener { task ->
-
-            if (task.isSuccessful) {
-                utility.toastln("User Added Successfully")
-            } else {
-                utility.toastln("Error Adding User")
-            }
-
-        }
-    }
-
-    private fun assignRandomToll() {
-        println("Getting tolls hopefully")
-        utility.getTollsForUser { tolls ->
-
-            val tollsList = ArrayList<Toll>()
-
-            tolls.forEach { toll ->
-                println(toll)
-                tollsList.add(toll)
-            }
-
-            retrieveGatesFromDatabase { gates ->
-                val userId: String = auth.currentUser!!.uid
-                val randomIndex: Int = Random.nextInt(gates.lastIndex+1)
-                val randomGate: Gate = gates[randomIndex]
-
-                val timestamp = Timestamp(System.currentTimeMillis())
-                val newToll = Toll(randomGate.id, timestamp)
-
-                tollsList.add(newToll)
-
-                usersReference.child(userId).child("tolls").setValue(tollsList).addOnCompleteListener { task ->
-                    if (!task.isSuccessful) {
-                        utility.toastln("Error Adding Random Toll")
-                    }
-                }
-                displayLatestToll()
-            addTextToTerminal("$${randomGate.cost} at ${randomGate.name}")
-            }
-        }
-
-    }
-
-    private fun displayLatestToll() {
-        retrieveGatesFromDatabase { gates ->
-            utility.getTollsForUser { tolls ->
-                val latestTollIndex: Int = tolls.lastIndex
-                val latestTollGateId: String = tolls[latestTollIndex].gateId
-                for (gate in gates) {
-                    if (gate.id == latestTollGateId) {
-                        utility.toastln("$${gate.cost} at ${gate.name}")
-                    }
-                }
-
-            }
-        }
-
-    }
-
-    private fun retrieveGatesFromDatabase(callback: (ArrayList<Gate>) -> Unit) {
-        val gatesList: ArrayList<Gate> = ArrayList<Gate>()
-
-        gatesReference.addListenerForSingleValueEvent(object: ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                gatesList.clear()
-
-                for (gateSnapshot in dataSnapshot.children) {
-                    val gate = gateSnapshot.getValue(Gate::class.java)
-                    gate?.let {
-                        gatesList.add(it)
-                    }
-                }
-                callback(gatesList)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
-    }
-
     override fun onResume() {
         super.onResume()
         val filter = IntentFilter()
