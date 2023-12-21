39de570d9103a9ea91c398ab83886c7a94151034
Eric McKevitt <erictmckevitt@gmail.com>
2023-12-21 17:49:05 -0600

    added basis for clearing toll data

--- app/src/main/java/com/shinetech/tollview/ViewActivity.kt
+++ app/src/main/java/com/shinetech/tollview/ViewActivity.kt
@@ -2,11 +2,22 @@ package com.shinetech.tollview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.shinetech.tollview.models.Toll
+import com.shinetech.tollview.util.Utility
 
 class ViewActivity : AppCompatActivity() {
+
+    private lateinit var utility: Utility
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view)
+
+        utility = Utility(applicationContext)
+
+        utility.getTollsForUser { tolls ->
+            var emptyTollsList: ArrayList<Toll> = ArrayList()
+
+        }
     }
 }
