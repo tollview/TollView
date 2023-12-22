5956bd55653caa85d54da58058c6dec78bd2a714
gdknas <mckevitt.r@gmail.com>
2023-12-21 18:03:59 -0600

    allegedly implemented toll wiping functionality on viewActivity

--- app/src/main/java/com/shinetech/tollview/ViewActivity.kt
+++ app/src/main/java/com/shinetech/tollview/ViewActivity.kt
@@ -2,22 +2,32 @@ package com.shinetech.tollview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.shinetech.tollview.models.Toll
 import com.shinetech.tollview.util.Utility
 
 class ViewActivity : AppCompatActivity() {
-
+    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val usersReference: DatabaseReference = database.reference.child("users")
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     private lateinit var utility: Utility
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view)
-
         utility = Utility(applicationContext)
 
-        utility.getTollsForUser { tolls ->
-            var emptyTollsList: ArrayList<Toll> = ArrayList()
-
-        }
+        val emptyTollsList: ArrayList<Toll> = ArrayList()
+        val userId = auth.currentUser!!.uid
+        println("attempting to wipe records of $userId")
+        usersReference.child(userId).child("tolls").setValue(emptyTollsList)
+            .addOnCompleteListener { task ->
+                if (!task.isSuccessful){
+                    utility.toast("Error deleting all the everything")
+                }
+            }
+        println("allegedly wiped records of $userId")
     }
 }
