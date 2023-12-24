d752a4554422319083ce71e34a82a80a1c97f8c0
Eric McKevitt <erictmckevitt@gmail.com>
2023-12-24 16:30:53 -0600

    safe deleted unused instance variables and associated imports

--- app/src/main/java/com/shinetech/tollview/ViewActivity.kt
+++ app/src/main/java/com/shinetech/tollview/ViewActivity.kt
@@ -2,23 +2,14 @@ package com.shinetech.tollview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import com.shinetech.tollview.models.Toll
 import com.shinetech.tollview.util.Utility
-import java.sql.Timestamp
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.time.ZoneOffset
-
 
 class ViewActivity : AppCompatActivity() {
-    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-    private val usersReference: DatabaseReference = database.reference.child("users")
-    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     private lateinit var utility: Utility
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,10 +30,10 @@ class ViewActivity : AppCompatActivity() {
                     tollDate.isAfter(startDate) && tollDate.isBefore(endDate.plusDays(1))
                 } ?: false // Handle cases where timestamp is null
             }
-            println("tolls between dates: $tollsBetweenDates")
-            println("number of tolls between dates: ${tollsBetweenDates.size}")
 
             // Use tollsBetweenDates as needed
+            println("tolls between dates: $tollsBetweenDates")
+            println("number of tolls between dates: ${tollsBetweenDates.size}")
         }
     }
 
